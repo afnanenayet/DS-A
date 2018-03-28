96a284846678beeaa8de7b817609eb09a582147c
Afnan Enayet <afnan.enayet@gmail.com>
2018-03-28 09:20:56 -0400

    Fix some formatting things

--- edit_distance.py
+++ edit_distance.py
@@ -3,6 +3,7 @@
 # A dynamic programming solution to finding the edit distance between two
 # strings
 
+
 def edit_distance(str1, str2):
     """ calculates the edit distance between 2 strings
     uses a DP approach with a table that calculates the edit distance between
@@ -27,24 +28,27 @@ def edit_distance(str1, str2):
     #   - an element is added to x
     #   - an element is changed from x
     #   - nothing changes bc current element is the same
-    for i in range(1, len(str1)+1):
-        for j in range(1, len(str2)+1):
+    for i in range(1, len(str1) + 1):
+        for j in range(1, len(str2) + 1):
             # if the characters are different, we know we will have to at least
             # change one thing
-            if str1[i-1] == str2[j-1]:
+            if str1[i - 1] == str2[j - 1]:
                 c = 0
             else:
                 c = 1
 
             # take the minimum case
             dp[i][j] = min(
-                dp[i-1][j] + 1,   # remove one char from str1
-                dp[i][j-1] + 1,   # remove one char from str2
-                dp[i-1][j-1] + c  # change one letter or don't change a letter (lengths stay the same)
+                dp[i - 1][j] + 1,   # remove one char from str1
+                dp[i][j - 1] + 1,   # remove one char from str2
+                # change one letter or don't change a letter (lengths stay the
+                # same)
+                dp[i - 1][j - 1] + c
             )
 
     # print min edit distance between str1[:len(str1)-1], str2[:len(str2)-1]
-    return dp[len(str1)-1][len(str2)-1]
+    return dp[len(str1) - 1][len(str2) - 1]
+
 
 # test
 pairs = [
@@ -55,4 +59,5 @@ pairs = [
 
 for pair in pairs:
     edit_dist = edit_distance(pair[0], pair[1])
-    print("Edit distance between " + pair[0] + " and " + pair[1] + " is " + str(edit_dist))
+    print("Edit distance between " +
+          pair[0] + " and " + pair[1] + " is " + str(edit_dist))
